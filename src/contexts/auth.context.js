1eac3b33b5403336693f0dcc7a0d73e441253228
Alvaro Barba <abarba.lod@gmail.com>
2023-10-17 01:37:08 +0200

    eslint comment

--- src/contexts/auth.context.js
+++ src/contexts/auth.context.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import { createContext, useEffect, useState } from "react"
 import authService from "../services/auth.services"
 
--- src/pages/RestaurantDetailsPage.jsx
+++ src/pages/RestaurantDetailsPage.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import { useContext, useEffect, useState } from "react"
 import { useParams, Link, useNavigate } from "react-router-dom"
 import restaurantsService from "../services/restaurants.services"
